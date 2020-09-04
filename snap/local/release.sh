3a54c22655c12d5e99be96a5e0a0899261c50322
vliedel <bart@crownstone.rocks>
2020-09-04 17:09:00 +0200

    Added bluetooth and removable media plugs. Release as stable.

--- snap/local/release.sh
+++ snap/local/release.sh
@@ -6,9 +6,11 @@ ARCH=$( uname -m )
 echo "ARCH: $ARCH"
 
 if [ "$ARCH" = "aarch64" ]; then
-        snapcraft upload --release edge *_arm64.snap
+#        snapcraft upload --release edge *_arm64.snap
+        snapcraft upload *_arm64.snap
 fi
 
 if [ "$ARCH" = "x86_64" ]; then
-        snapcraft upload --release edge *_amd64.snap
+#        snapcraft upload --release edge *_amd64.snap
+        snapcraft upload *_amd64.snap
 fi
