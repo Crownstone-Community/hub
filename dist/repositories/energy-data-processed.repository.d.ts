5bddbf5981178d3338272850c19082cea5fe452d
alexdm0 <alexdemulder@gmail.com>
2020-09-18 15:19:00 +0200

    added missing modules

---
+++ dist/repositories/energy-data-processed.repository.d.ts
@@ -0,0 +1,6 @@
+import { DefaultCrudRepository, juggler } from '@loopback/repository';
+import { EnergyDataProcessed } from '../models';
+export declare class EnergyDataProcessedRepository extends DefaultCrudRepository<EnergyDataProcessed, typeof EnergyDataProcessed.prototype.id> {
+    protected datasource: juggler.DataSource;
+    constructor(datasource: juggler.DataSource);
+}
---
+++ dist/repositories/energy-data-processed.repository.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EnergyDataProcessedRepository = void 0;
+const tslib_1 = require("tslib");
+const repository_1 = require("@loopback/repository");
+const core_1 = require("@loopback/core");
+const models_1 = require("../models");
+let EnergyDataProcessedRepository = class EnergyDataProcessedRepository extends repository_1.DefaultCrudRepository {
+    constructor(datasource) {
+        super(models_1.EnergyDataProcessed, datasource);
+        this.datasource = datasource;
+        this.datasource.autoupdate();
+    }
+};
+EnergyDataProcessedRepository = tslib_1.__decorate([
+    tslib_1.__param(0, core_1.inject('datasources.mongo')),
+    tslib_1.__metadata("design:paramtypes", [repository_1.juggler.DataSource])
+], EnergyDataProcessedRepository);
+exports.EnergyDataProcessedRepository = EnergyDataProcessedRepository;
+//# sourceMappingURL=energy-data-processed.repository.js.map
