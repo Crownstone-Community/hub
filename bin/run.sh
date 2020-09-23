183025ba57b27d1e6be8d1fb85d1e759256c3db0
vliedel <bart@crownstone.rocks>
2020-09-23 15:29:49 +0200

    [snap] No need to stream output to file.

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sleep 2s
-$SNAP/bin/node $SNAP/execute.js >> $SNAP_DATA/debug.log 2>&1
+#sleep 2s
+#$SNAP/bin/node $SNAP/execute.js >> $SNAP_DATA/debug.log 2>&1
+$SNAP/bin/node $SNAP/execute.js
