66a3bb3abf58e0c249c9a4077725e485b9081d26
alexdm0 <alexdemulder@gmail.com>
2020-09-29 20:27:43 +0200

    cleaned up the unused files

--- dist/controllers/debug/database-dump.controller.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class DatabaseDumpController {
-    constructor();
-    dumpHubDatabase(): Promise<any>;
-    dumpUserDatabase(): Promise<any>;
-    dumpUserPermissionsDatabase(): Promise<any>;
-    dumpSphereFeatureDatabase(): Promise<any>;
-}
--- dist/customRoutes/Visualization.d.ts
+++
@@ -1,3 +0,0 @@
-import express from 'express';
-import { CrownstoneHubApplication } from '../application';
-export declare function addVisualizationRoutes(app: express.Application, loopbackApp: CrownstoneHubApplication): void;
