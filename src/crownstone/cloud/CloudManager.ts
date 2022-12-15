c7564cb5824447f6c203f82673492590875f0cd3
vliedel <bart@crownstone.rocks>
2022-12-15 16:12:47 +0100

    Added login url as env var.

--- src/crownstone/cloud/CloudManager.ts
+++ src/crownstone/cloud/CloudManager.ts
@@ -313,7 +313,8 @@ export class CloudManager {
     if (this.sse === null) {
       this.sse = new CrownstoneSSE({
         sseUrl: process.env.SSE_URL,
-        hubLoginBase: process.env.hubLoginBase ?? 'https://cloud.crownstone.rocks/api/Hubs/',
+        loginUrl: process.env.LOGIN_URL,
+        hubLoginBase: process.env.hubLoginBase,
         autoreconnect: false,
         projectName:'crownstoneHub'
       });
